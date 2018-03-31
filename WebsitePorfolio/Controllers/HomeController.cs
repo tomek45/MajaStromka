4656d5cf099466752b173707d75de1b04e816c0f
tomek45 <tomasz.misiejuk@gmail.com>
2018-03-31 20:35:55 +0200

    Initial commit

---
+++ WebsitePorfolio/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsitePorfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Change(String LanguageAbbrevation)
+        {
+            if (LanguageAbbrevation != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(LanguageAbbrevation);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(LanguageAbbrevation);
+            }
+
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = LanguageAbbrevation;
+            Response.Cookies.Add(cookie);
+
+            return View("Index");
+        }
+    }
+}
---
+++ WebsitePorfolio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application CodeFile="Global.asax.cs" Inherits="WebsitePorfolio.MvcApplication" Language="C#" %>
